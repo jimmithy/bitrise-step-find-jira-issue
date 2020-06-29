4b1566842f7147cbdad6fd65f1d145f56c08c9ed
James O'Brien <jobrien@themeetgroup.com>
2020-06-29 16:09:06 -0400

    Pull request feedback
    * JIRA_ISSUE_LIST
    * More descriptive input title
    * Input variable should be lower snake case
    * input variable shouldn't contain the word input.

--- step.sh
+++ step.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 set -o pipefail
 
-echo "Extracting JIRA Issue number from: '${FIND_ISSUE_INPUT}'"
+echo "Extracting JIRA Issue number from: '${find_issue_content}'"
 
 # Identify Jira Issue, if multiple use | as delimiter
-JIRA_ISSUE="$(echo "${FIND_ISSUE_INPUT}" | egrep -o '[a-zA-Z]+-[0-9]+' | sort -uf | tr '\n' '|' | sed 's/.$//')"
+JIRA_ISSUE_LIST="$(echo "${find_issue_content}" | egrep -o '[a-zA-Z]+-[0-9]+' | sort -uf | tr '\n' '|' | sed 's/.$//')"
 
 # If no issue, abort
-if [ -z "${JIRA_ISSUE}" ]; then
+if [ -z "${JIRA_ISSUE_LIST}" ]; then
     echo "Failure: Input doesn't contain JIRA issue"
     exit -1
 fi
 
 # Set as enviornment variable
 if command -v envman 2>/dev/null; then
-    echo "Adding \"${JIRA_ISSUE}\" to env. vars"
-    envman add --key JIRA_ISSUE --value "${JIRA_ISSUE}"
+    echo "Adding \"${JIRA_ISSUE_LIST}\" to env. vars"
+    envman add --key JIRA_ISSUE_LIST --value "${JIRA_ISSUE_LIST}"
 fi
